47aea7cafb7607b5ef9d555d382a9995986066a9
Engin Diri <engin.diri@mail.schwarz>
2021-12-07 22:31:06 +0100

    feat: add .golangci.yaml and fix issues
    Signed-off-by: Engin Diri <engin.diri@mail.schwarz>

--- cmd/minecraftExporter/main.go
+++ cmd/minecraftExporter/main.go
@@ -1,7 +1,10 @@
-package minecraft_exporter
+package minecraftExporter
 
 import (
-	"github.com/go-kit/kit/log/level"
+	"net/http"
+	"os"
+
+	"github.com/go-kit/log/level"
 	"github.com/minecraft-exporter/pkg/config"
 	"github.com/minecraft-exporter/pkg/exporter"
 	"github.com/minecraft-exporter/pkg/template"
@@ -12,12 +15,9 @@ import (
 	"github.com/prometheus/common/version"
 	"github.com/prometheus/exporter-toolkit/web"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"net/http"
-	"os"
 )
 
 func Run() {
-
 	config := config.NewConfg()
 	promlogConfig := &promlog.Config{
 		Level: &promlog.AllowedLevel{},
@@ -30,8 +30,8 @@ func Run() {
 
 	config.LoadFile()
 
-	level.Info(logger).Log("msg", "Starting minecraft_exporter", "version", version.Info())
-	level.Info(logger).Log("msg", "Build context", "build", version.BuildContext())
+	level.Info(logger).Log("msg", "Starting minecraft_exporter", "version", version.Info()) //nolint:errcheck
+	level.Info(logger).Log("msg", "Build context", "build", version.BuildContext())         //nolint: errcheck
 
 	prometheus.MustRegister(version.NewCollector("minecraft_exporter"))
 	prometheus.MustRegister(exporter.New(*config.RconAddress, *config.RconPassword, *config.WorldPath, *config.NameSource, config.DisabledMetrics, logger))
@@ -39,10 +39,13 @@ func Run() {
 	http.Handle(*config.MetricsPath, promhttp.Handler())
 	template := template.NewIndexTemplate()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template.Execute(w, config)
+		err := template.Execute(w, config)
+		if err != nil {
+			level.Error(logger).Log("msg", "Error executing template", "err", err)
+		}
 	})
 
-	level.Info(logger).Log("msg", "Listening on address", "address", *config.ListenAddress)
+	level.Info(logger).Log("msg", "Listening on address", "address", *config.ListenAddress) //nolint:errcheck
 	srv := &http.Server{Addr: *config.ListenAddress}
 	if err := web.ListenAndServe(srv, *config.WebConfig, logger); err != nil {
 		level.Error(logger).Log("msg", "Error running HTTP server", "err", err)
